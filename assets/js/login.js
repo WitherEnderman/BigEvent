9cfde8855709d27b589b18d62249ec6755dc1a9c
wfx <heiyuxuan@qq.com>
2021-01-03 10:53:50 +0800

    表单注册登录

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    $(function () {
+        // 点击“去注册账号”的链接
+        $('#link_reg').on('click', function () {
+            $('.login-box').hide()
+            $('.reg-box').show()
+        })
+
+        // 点击“去登录”的链接
+        $('#link_login').on('click', function () {
+            $('.login-box').show()
+            $('.reg-box').hide()
+        })
+    })
+}
