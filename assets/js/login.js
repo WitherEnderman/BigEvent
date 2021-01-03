b076909db9a6d10143276003b67917a0518e5b6f
wfx <heiyuxuan@qq.com>
2021-01-03 19:22:54 +0800

    注册跳转登录跳转主页

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,26 +1,20 @@
-window.onload = function () {
-    // START
-    // 给注册登录按钮绑定点击事件
-    $(function () {
-        // 点击“去注册账号”的链接
-        $('#link_reg').on('click', function () {
-            $('.login-box').hide()
-            $('.reg-box').show()
-        })
-
-        // 点击“去登录”的链接
-        $('#link_login').on('click', function () {
-            $('.login-box').show()
-            $('.reg-box').hide()
-        })
+$(function () {
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
     })
 
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
 
-    // 通过`form.verify()` 函数自定义校验规则，里面是`key：value`形式，
-    // key后续对应设置到标签的`lay-verity`属性中，`value`就是验证的规则，
-    // 这里定义了两个自定义校验规则，一个是密码框，利用的是正则，一个是确认密码
+    // 从 layui 中获取 form 对象
     var form = layui.form
-
+    var layer = layui.layer
+    // 通过 form.verify() 函数自定义校验规则
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -36,21 +30,49 @@ window.onload = function () {
             }
         }
     })
-  
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-}//  onloda函数  
+})
